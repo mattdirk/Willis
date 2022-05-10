88c9ea98b344d8e6cd85c326277bde845980a22b
mbodily <Matt.Bodily@radian.com>
2022-05-10 17:21:02 -0600

    solution for uniqueEmailAddresses

--- CSharp/UniqueEmailAddresses.Tests/UniqueEmailAddressesTests.cs
+++ CSharp/UniqueEmailAddresses.Tests/UniqueEmailAddressesTests.cs
@@ -8,7 +8,12 @@ namespace UniqueEmailAddresses.Tests
         [Fact]
         public void Test_A()
         {
-
+            string[] emails = new string[] { "team1@somewhere.com", "team.1+bob@somewhere.com", "team1+jill+bob@somewhere.com" };
+            int result = UniqueEmailAddresses.NumberOfUniqueEmailAddresses(emails);
+            Assert.Equal(1, result) ;
+            emails = new string[] { "team2@somewhere.com", "team2@some.where.com" };
+            result = UniqueEmailAddresses.NumberOfUniqueEmailAddresses(emails);
+            Assert.Equal(2, result);
         }
     }
 }
--- CSharp/UniqueEmailAddresses/UniqueEmailAddresses.cs
+++ CSharp/UniqueEmailAddresses/UniqueEmailAddresses.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UniqueEmailAddresses
 {
@@ -6,7 +8,31 @@ namespace UniqueEmailAddresses
     {
         public static int NumberOfUniqueEmailAddresses(string[] emails)
         {
-            throw new NotImplementedException();
+            List<string> cleanEmails = new List<string>();
+            try
+            {
+                foreach(string email in emails)
+                {
+                    string[] parts = email.Split("@");
+                    if(parts.Length > 2)
+                    {
+                        continue;
+                    }
+                    string clean = parts[0].Replace(".", "");
+                    int firstIndex = clean.IndexOf("+");
+                    if(firstIndex > -1)
+                    {
+                        clean = clean.Substring(0, firstIndex);
+                    }
+                    cleanEmails.Add(clean + "@" + parts[1]);
+                }
+                List<string> distinct = new List<string>(cleanEmails.Distinct());
+                return distinct.Count;
+            }catch(Exception ex)
+            {
+
+            }
+            return 0;
         }
     }
 }
